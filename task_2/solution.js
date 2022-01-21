0c7dcb0a5e4d3952896aefb8c29d570f14e0ddf2
TatyanaRogova <rogova.ts@gmail.com>
2022-01-21 16:17:31 +0400

    commit changes

--- task_2/solution.js
+++ task_2/solution.js
@@ -19,6 +19,7 @@ function calcShipping(sum, min, shipping) {
     // то shippingSum присвоить значение shippingPrice
     // то shippingSum присвоить значение shippingPrice
     // то shippingSum присвоить значение shippingPrice
+    // то shippingSum присвоить значение shippingPrice
     
 
     // Конец решения задания №2.1.
