4fa3550b2af36e2376be2f6351678377f2ea9de4
TatyanaRogova <rogova.ts@gmail.com>
2022-02-11 12:05:14 +0400

    task_final question

--- task_final/solution.js
+++ task_final/solution.js
@@ -3,31 +3,32 @@
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order:{}};
 
-    let countOfGoods = goods.length;
+    let a = goods.count;
+    let b = goods.title;
 
-    for (let i = 0; i < countOfGoods; i++) {
-        data.goods.push(goods[i].title, goods[i].count);
-    }
-    //for (let property in data.goods){
-    //    data.goods.push('${property}, ${data.goods[property]}')
+    data.goods = {title: b, count: a};
+    
+    let countOfGoods = goods.length;
+    
+    for(let i = 0; i <countOfGoods; i++){
+        function dataGoodsPush(i);
+    };
+
+    //for (let nameGood of data.goods){
+    //    data.goods.push(nameGood)
     //};
 
-    data.client = [name + phone];
+    data.client = name + + phone;
 
     data.order.address = {
-        street: data.order.address.street,
-        house: data.order.address.house,
-        entrance: data.order.address.entrance,
-        floor: data.order.address.floor,
-        flat: data.order.address.flat
+        street: "",
+        house: "",
+        entrance: "",
+        floor: "",
+        flat: ""
       };
 
-    data.order.sum = [900, 1800]; //name + phone + address + goods + sum;
-
-    data.goods = {
-        title: goods.title,
-        count: goods.count
-    };
+    data.order.sum = sum;
 
     let jsonData = JSON.stringify({data: data});
 
